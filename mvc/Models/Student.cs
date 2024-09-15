5e5e0d8803840c9be76687c6ab4e39421d76f93b
Recep Pazarlı <receppazarli99@hotmail.com>
2024-09-15 13:39:00 +0300

    ..

--- mvc/Models/Student.cs
+++ mvc/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +10,15 @@ namespace mvc.Models
     public class Student
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad gereklidir.")]
         public string Name { get; set; }
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Tc Kimlik No eksik olamaz.")]
+        [DisplayName("Türkiye Cumhuriyeti Kimlik Numarası")]
+        [MinLength(11, ErrorMessage = "11 karakterden kısa olamaz.")]
+        [MaxLength(11, ErrorMessage = "11 karakterden uzun olamaz.")]
         public string IdentityNumber { get; set; }
         public string DepartmentName { get; set; }
         public int EntryYear { get; set; }
